df3940202f5c1aa6acf2abfa71e2b0f4c5fa952a
Benjamin <benjaminayimah@gmail.com>
2023-03-30 13:14:31 +0000

    bottom nav

--- src/store/index.js
+++ src/store/index.js
@@ -4,7 +4,7 @@ export default createStore({
   state: {
     iphone: false,
     phone: false,
-    currentPlayer: { active: true, player: {id: 1, trackname: 'Forever', artist: 'Lewis Capaldi', liked: false, image: require('@/assets/images/artist-1.png'), bgColor: '#02264A'} }
+    currentPlayer: { active: true, play: false, player: {id: 1, trackname: 'Forever', artist: 'Lewis Capaldi', liked: false, image: require('@/assets/images/artist-1.png'), bgColor: '#02264A'} }
   },
   mutations: {
     computeInit(state) {
@@ -12,7 +12,17 @@ export default createStore({
         state.iphone = true
       }
       ( /iPhone|iPad|android /i.test(navigator.userAgent) ) ? state.phone = true: state.phone = false
-    }
+    },
+    toggleLike(state) {
+      state.currentPlayer.player.liked = !state.currentPlayer.player.liked
+    },
+    playPause(state) {
+      if (state.currentPlayer.play) {
+        state.currentPlayer.play = false
+      } else {
+        state.currentPlayer.play = true
+      }
+    },
   },
   actions: {
   },
