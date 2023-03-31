188ce676b861219b96a302bd829e77827d2c237b
Benjamin <benjaminayimah@gmail.com>
2023-03-31 11:48:38 +0000

    bottom nav

--- src/store/index.js
+++ src/store/index.js
@@ -3,6 +3,10 @@ import { createStore } from 'vuex'
 export default createStore({
   state: {
     iphone: false,
+    categories: [
+      {id: 1, name: 'music'},
+      {id: 2, name: 'podcasts & shows'}
+    ],
     currentPlayer: { active: true, play: false, player: {id: 1, trackname: 'Forever', artist: 'Lewis Capaldi', artist_id: 1, liked: false, image: require('@/assets/images/artist-1.png'), bgColor: '#02264A'} },
     artists: [
       {id: 1, name: 'Lewis Capaldi', avatar: require('@/assets/images/lewis_capaldi.png'), stream: 29938 },
@@ -13,9 +17,9 @@ export default createStore({
       {id: 6, name: 'Black Sherif', avatar: require('@/assets/images/black_sherif.png'), stream: 9738310 },
     ],
     discover: [
-      {id: 1, title: 'for you', playlist: []},
-      {id: 2, title: 'new releases', playlist: []},
-      {id: 3, title: 'rise and shine', playlist: []},
+      {id: 1, title: 'for you', category: 'music', playlist: []},
+      {id: 2, title: 'new releases', category: 'podcast', playlist: []},
+      {id: 3, title: 'rise and shine', category: 'music', playlist: []},
 
     ]
   },
@@ -42,7 +46,8 @@ export default createStore({
     getiPhone: (state) => state.iphone,
     getCurrentPlayer: (state) => state.currentPlayer,
     getArtists: (state) => state.artists,
-    getDiscover: (state) => state.discover
+    getDiscover: (state) => state.discover,
+    getCategories: (state) => state.categories
   },
   modules: {
   }
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -7,9 +7,7 @@
   </teleport>
   <div class="home">
     <nav class="flx gap-8 ai-c sticky-top blur p-l-r-16">
-      <li><router-link to="/" class="flx ai-c scale-2">Music</router-link></li>
-      <li><router-link to="/" class="flx ai-c scale-2">Podcasts</router-link></li>
-      <li><router-link to="/" class="flx ai-c scale-2">Shows</router-link></li>
+      <button v-for="category in getCategories" :key="category.id" class="flx ai-c scale-2 capitalize">{{ category.name }}</button>
     </nav>
     <main class="p-l-r-16">
       <home-section-1 />
@@ -23,11 +21,13 @@ import WinTopMixin from '../mixins/WinTopMixin';
 import HomeSection1 from '../components/includes/HomeSection1.vue'
 import HomeTopNav from '../components/includes/HomeTopNav.vue'
 import HomeSection2 from '../components/includes/HomeSection2.vue';
+import { mapGetters } from 'vuex';
 // @ is an alias to /src
 export default {
   components: { HomeTopNav, HomeSection1, HomeSection2 },
   name: 'Home',
-  mixins: [WinTopMixin]
+  mixins: [WinTopMixin],
+  computed: mapGetters(['getCategories'])
 }
 </script>
 <style lang="scss" scoped>
@@ -35,10 +35,13 @@ nav{
   height: 50px;
   background-color: rgba(10, 11, 13, 0.7);
 }
-a{
+button{
   background-color: $secondary-bg;
   padding: 8px 16px;
   border-radius: 20px;
   border: 1px solid rgba(69, 70, 75, 0.5);
+  color: $white;
+  height: 40px;
+  font-size: inherit;
 }
 </style>
